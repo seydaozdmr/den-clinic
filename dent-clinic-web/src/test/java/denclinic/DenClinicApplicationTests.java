1cec9de67f52b6a6ff05160ae8d38af7308b1da3
tableonthewall <seydaozdemir07@gmail.com>
2020-09-04 15:09:24 +0300

    Dentists Controller Closes #4

---
+++ dent-clinic-web/src/test/java/denclinic/DenClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package denclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
