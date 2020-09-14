33db3fa843643c3ef00b8a16427c68668ceebddd
tableonthewall <seydaozdemir07@gmail.com>
2020-09-14 14:40:45 +0300

    Create dentist speciality Entity, Associate to Dentist Closes #20

--- dent-clinic-data/src/main/java/org/denclinic/denclinic/model/Dentist.java
+++ dent-clinic-data/src/main/java/org/denclinic/denclinic/model/Dentist.java
@@ -1,5 +1,13 @@
 package org.denclinic.denclinic.model;
 
 public class Dentist extends Person {
+    private Speciality speciality;
 
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
 }
