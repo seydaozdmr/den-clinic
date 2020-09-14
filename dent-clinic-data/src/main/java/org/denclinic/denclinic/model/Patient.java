a2cc45cb0ce34d347877ed9dfe70c95d91abf05c
tableonthewall <seydaozdemir07@gmail.com>
2020-09-14 14:45:43 +0300

    Add contact info properties to Patient Closes #21

--- dent-clinic-data/src/main/java/org/denclinic/denclinic/model/Patient.java
+++ dent-clinic-data/src/main/java/org/denclinic/denclinic/model/Patient.java
@@ -3,7 +3,34 @@ package org.denclinic.denclinic.model;
 import java.util.Set;
 
 public class Patient extends Person {
+    private String adress;
+    private String city;
+    private String telephone;
     private Set<Tooth> tooths;
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Tooth> getTooths() {
         return tooths;
@@ -12,6 +39,4 @@ public class Patient extends Person {
     public void setTooths(Set<Tooth> tooths) {
         this.tooths = tooths;
     }
-
-
 }
