31ebe203046dc02cfd7d64028b4f174cb2ccc757
tableonthewall <seydaozdemir07@gmail.com>
2020-10-29 13:34:47 +0300

    Convert Visit to JPA Entities Closes #31

---
+++ dent-clinic-data/src/main/java/org/denclinic/denclinic/repositories/PatientRepositories.java
@@ -0,0 +1,7 @@
+package org.denclinic.denclinic.repositories;
+
+import org.denclinic.denclinic.model.Patient;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientRepositories extends CrudRepository<Patient,Integer> {
+}
