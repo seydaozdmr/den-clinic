030cf13b4f9ea3edf9f460eb4417c9fcc7a0c0ed
tableonthewall <seydaozdemir07@gmail.com>
2020-10-29 23:34:02 +0300

    Create Spring Data JPA Service for Visit #39

--- dent-clinic-data/src/main/java/org/denclinic/denclinic/services/map/VisitMapService.java
+++ dent-clinic-data/src/main/java/org/denclinic/denclinic/services/map/VisitMapService.java
@@ -2,11 +2,13 @@ package org.denclinic.denclinic.services.map;
 
 import org.denclinic.denclinic.model.Visit;
 import org.denclinic.denclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class VisitMapService extends AbstractMapService<Visit,Integer> implements VisitService {
     @Override
     public Set<Visit> findAll() {
