b4c4ffc17fa93d53fa9ffb27c00390da9a135747
tableonthewall <seydaozdemir07@gmail.com>
2021-10-26 13:06:23 +0300

    26/10/2021 crud service

--- dent-clinic-data/src/main/java/org/denclinic/services/CrudService.java
+++ dent-clinic-data/src/main/java/org/denclinic/services/CrudService.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 public interface CrudService<T,ID> {
     //T tipindeki elemenların listelenmesi, id'ye göre bulunması, kaydedilmesi,silinmesi
+    // service yapılarında tekrar eden yapıların genel olarak bir interface altında ifade edilmesi
     Set<T> findAll();
     T findById(ID id);
     T save(T object);
